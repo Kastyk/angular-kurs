f28fb581921e3e98f964ee15218360ced4db65cd
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-09-25 23:40:19 +0200

    Lessons about components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { Test2Component } from './test2/test2.component';
+import { Test3Component } from './test3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    // tablica komponentów, pipe'ów i dyrektyw
+    AppComponent,
+    TestComponent,
+    Test2Component,
+    Test3Component,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, // moduł używany do odpalenie aplikacji w przeglądarce
+    AppRoutingModule, // moduł używany do routingu
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // tutaj podajemy informacje o serwisach
+  bootstrap: [AppComponent], // komponent startowy
 })
-export class AppModule { }
+export class AppModule {}
