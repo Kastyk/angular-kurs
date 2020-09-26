e107ff4876a61aa6f0debaf155b3be3bc41df0bc
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-09-26 16:26:57 +0200

    ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,22 +16,4 @@ export class AppComponent {
     'Sobota',
     'Niedziela',
   ];
-  dogs: Dog[] = [];
-  constructor() {}
-
-  addDogs = () => {
-    return this.dogs.push(
-      new Dog('Reksio', 4),
-      new Dog('Łatek', 3),
-      new Dog('Maksiu', 5),
-      new Dog('Łapek', 6),
-      new Dog('Pluto', 8),
-    );
-  };
-  removeDogs = () => {
-    return (this.dogs = []);
-  };
-}
-class Dog {
-  constructor(public name: string, public age: number) {}
 }
