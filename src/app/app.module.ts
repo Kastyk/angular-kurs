ca978059f613abe24635634fea599e8c3b252ab5
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-09-26 18:28:24 +0200

    Add FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { NowyComponent } from './nowy/nowy.component';
   imports: [
     BrowserModule, // moduł używany do odpalenie aplikacji w przeglądarce
     AppRoutingModule, // moduł używany do routingu
+    FormsModule,
   ],
   providers: [], // tutaj podajemy informacje o serwisach
   bootstrap: [AppComponent], // komponent startowy
