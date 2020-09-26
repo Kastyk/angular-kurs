531654e78e6878079fa08ba108e56a2f941aa6a4
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-09-26 16:16:49 +0200

    ngFor, ngIf,ng- template i ng-container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angulara';
-  pi = Math.PI;
-  date = new Date();
-  dog = new Dog('Reksio', 4);
-  dog1: Dog;
-  showDog = () => `Mój pies to ${this.dog.name} i ma ${this.dog.age} lata.`;
+  days = [
+    'Poniedziałek',
+    'Wtorek',
+    'Środa',
+    'Czwartek',
+    'Piątek',
+    'Sobota',
+    'Niedziela',
+  ];
+  dogs: Dog[] = [];
+  constructor() {}
+
+  addDogs = () => {
+    return this.dogs.push(
+      new Dog('Reksio', 4),
+      new Dog('Łatek', 3),
+      new Dog('Maksiu', 5),
+      new Dog('Łapek', 6),
+      new Dog('Pluto', 8),
+    );
+  };
+  removeDogs = () => {
+    return (this.dogs = []);
+  };
 }
 class Dog {
   constructor(public name: string, public age: number) {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NowyComponent } from './nowy/nowy.component';
 
 @NgModule({
   declarations: [
     // tablica komponentów, pipe'ów i dyrektyw
     AppComponent,
+    NowyComponent,
   ],
   imports: [
     BrowserModule, // moduł używany do odpalenie aplikacji w przeglądarce
